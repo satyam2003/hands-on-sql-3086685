64b7ab8472f30e01c8aceccfafe6d40c6d674746
satyam2003 <49554787+satyam2003@users.noreply.github.com>
2024-01-04 20:27:24 +0000

    git commit -m “Added comments”

--- queries/01_01/SELECTStatement.sql
+++ queries/01_01/SELECTStatement.sql
@@ -2,4 +2,7 @@ SELECT * FROM emp_tab;
 
 SELECT empno,name FROM emp_tab;
 
+/*Using Alias for Annual Salary 
+Important to note that if Alias name as space then enclose the name 
+inside single quotes*/
 SELECT empno,name,salary*12 'Annual Salary' FROM emp_tab;
